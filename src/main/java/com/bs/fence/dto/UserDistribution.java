69ba21fdc2c3da3331e3a42afce8e698d2c157d3
Sjx <1450320103@qq.com>
2023-04-12 23:47:21 +0800

    部分代码优化

---
+++ src/main/java/com/bs/fence/dto/UserDistribution.java
@@ -0,0 +1,20 @@
+package com.bs.fence.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author sjx
+ * @Description TODO
+ * @create 2023-04-11-17:51
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDistribution {
+
+    private int areaId;
+    private int count;
+    private int areaName;
+}
